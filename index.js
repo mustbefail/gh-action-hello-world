b7ddc88fb25cbe333ed5adfe98c92f826227b1e6
Viktor Kushnir <mustbefail@gmail.com>
2021-08-26 19:31:42 +0300

    change name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { helloWorld } = require('helloWorld.js');
 
 exports.handler = async (event) => {
-  const message = helloWorld();
+  const message = helloWorld('AWS');
   const response = {
     statusCode: 200,
     body: JSON.stringify(message),
